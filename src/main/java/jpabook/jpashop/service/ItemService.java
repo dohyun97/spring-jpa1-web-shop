f3af8c58bea209a15fe8dc4d12af17e3c7551b55
dohyun97 <dohyun647@gmail.com>
2023-04-27 14:07:47 -0400

    item side

---
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -0,0 +1,29 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class ItemService {
+    private final ItemRepository repository;
+
+    @Transactional
+    public void saveItem(Item item){
+        repository.save(item);
+    }
+
+    public List<Item> findItems(){
+        return repository.findAll();
+    }
+    public Item findOne(Long itemId){
+        return repository.findOne(itemId);
+    }
+}
+
